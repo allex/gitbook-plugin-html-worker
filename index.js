3c62e37ca23e7777258e37e9fb4282e2073a427f
Allex Wang <allex.wxn@gmail.com>
2018-04-18 10:45:22 +0800

    Fix gitbook build cli with manually workspace. support async hook api.

--- index.js
+++ index.js
@@ -7,19 +7,23 @@
  *   Allex Wang <allex.wxn@gmail.com> (http://iallex.com/)
  */
 
-const pluginName = 'html-worker'
 const fs = require('fs')
 const os = require('os')
 const path = require('path')
+const util = require('util')
+const readdir = util.promisify(fs.readdir)
+const writeFile = util.promisify(fs.writeFile)
 
 const htmlMinifier = require('html-minifier').minify
 const readFileSync = f => fs.readFileSync(f, 'utf8')
+
+const pluginName = 'html-worker'
 
 class Worker {
   constructor (book, cfg) {
     this.rt = book
     this.cfg = cfg
-    this.root = book.config.values.root
+    this.root = book.resolve('')
     this.output = book.output.root()
 
     this.logger = ['debug', 'info', 'warn', 'error'].reduce((o, m) => {
@@ -31,6 +35,8 @@ class Worker {
       const outroFile = path.resolve(this.root, cfg.outro)
       if (fs.existsSync(outroFile)) {
         this._outroContent = readFileSync(outroFile).trim()
+      } else {
+        this.logger.warn('outro file not exists "' + outroFile + '"')
       }
     }
   }
@@ -64,28 +70,30 @@ class Worker {
   }
 
   minifyHtml () {
-    const walkDir = (dirPath, book) => {
-      fs.readdir(dirPath, (err, files) => {
-        if (err) throw err
-        files.forEach(f => {
-          f = path.join(dirPath, f)
-          const stat = fs.statSync(f)
-          if (stat.isFile() && f.match(/\.html$/) !== null) {
-            const html = fs.readFileSync(f, 'utf8').replace(/^\s+|\s+$/mg, '')
-            fs.writeFileSync(f, this.processHtml(html, f))
-            this.logger.info('process => "' + path.relative(this.output, f) + '"')
-          } else if (stat.isDirectory()) {
-            walkDir(f, book)
-          }
-        })
-      })
-    }
-
-    walkDir(this.output, this.rt)
+    const walkDir = dirPath =>
+      readdir(dirPath).then(files =>
+        Promise.all(
+          files.map(f => {
+            f = path.join(dirPath, f)
+            const stat = fs.statSync(f)
+            if (stat.isFile() && f.match(/\.html$/) !== null) {
+              const html = fs.readFileSync(f, 'utf8').replace(/^\s+|\s+$/mg, '')
+              return writeFile(f, this.processHtml(html, f))
+                .then(result => {
+                  this.logger.info('process => "' + path.relative(this.output, f) + '"')
+                  return f
+                })
+            } else if (stat.isDirectory()) {
+              return walkDir(f)
+            }
+          })
+        )
+      ).catch(err => Promise.reject(err))
+    return walkDir(this.output, this.rt)
   }
 
   build () {
-    this.minifyHtml()
+    return this.minifyHtml()
   }
 }
 
@@ -95,7 +103,7 @@ module.exports = {
       const cfg = this.config.get('pluginsConfig')[pluginName]
       const worker = new Worker(this, cfg)
       try {
-        worker.build()
+        return worker.build()
       } catch (e) {
         console.log(e.stack)
       }
